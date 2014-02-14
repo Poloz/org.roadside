f003507708917fb4787aeece9633de0a50a9e63a
Poloz <underpoloz@gmail.com>
2014-02-14 13:13:36 +0400

    Example 1

---
+++ src/org/roadside/ex1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.roadside;
+
+/**
+ *
+ * @author poloz@home
+ */
+public class ex1 {
+        public int i;
+        public char ch;     
+}
