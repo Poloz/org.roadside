3db3e564d3137da8eba026c8c62d895f675be2d2
Poloz <underpoloz@gmail.com>
2014-02-19 17:15:24 +0400

    Fuck it, i begin.)

---
+++ src/org/roadside/being.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.roadside;
+
+/**
+ *
+ * @author poloz@home
+ */
+public class being {
+    public int hitPoints;
+    public int attrOne;
+    public int attrTwo;
+    public int attrThree;
+    public int attrFour;
+    public int attrFive;
+    public int attrSix;
+    public int attrSeven;
+}
