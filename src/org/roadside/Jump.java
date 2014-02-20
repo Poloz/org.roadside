e553eb8f1002b59185cb1b2a95b191717ca2533f
Poloz <underpoloz@gmail.com>
2014-02-20 13:37:44 +0400

    Fuck it, i begin again.)

--- src/org/roadside/Jump.java
+++ src/org/roadside/Jump.java
@@ -1,49 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- *//*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.roadside;
 
-import exercises1.Ex1Execute;
 
 /**
  *
- * @author poloz@home
- * 
- * You can <em>even</em> insert a list:
- * 
- * <ol>
- * <li> Item one
- * <li> Item two
- * <li> Item three
- * </ol>
- * 
+ * @author Poloz
+ * @version 1
  */
 public class Jump {    
 
     /**
      * @param args the small command line arguments
-     * @author poloz@home
-     * @version 4.0
      */
     public static void main(String[] args) {
         // TODO code application logic here
         
-       /**
-        * Вся дребедень в этом комментарии - выполнение упражнений 1 главы
-        * Thinking in Java
-        *        Ex1Execute e1 = new Ex1Execute();
-        *        e1.Execute();
-        *        System.out.println(args[0]);
-        *        System.out.println(args[1]);
-        *        System.out.println(args[2]);
-        */ 
 
     }
            
