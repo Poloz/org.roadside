4ff92db9392ff713b160247ebae2a2d8c770b9ff
Poloz <underpoloz@gmail.com>
2014-02-20 10:34:38 +0400

    Another.

---
+++ src/exercises1/Ex1Test.java
@@ -0,0 +1,23 @@
+package exercises1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author poloz@home
+ */
+public class Ex1Test {
+        private Ex1 exercise1;
+        
+        public void Execute(){
+        exercise1 = new Ex1();
+        System.out.println("Exercise 1 start:");
+        System.out.println(exercise1.i);
+        System.out.println(exercise1.ch);
+        System.out.println("That was incredible, thanks. Ex1 ends here.");
+        }
+}
---
+++ src/exercises1/Ex4DataOnly.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package exercises1;
+
+/**
+ *
+ * @author poloz@home
+ */
+public class Ex4DataOnly {
+    public int i;
+    public double d;
+    public boolean b;
+}
