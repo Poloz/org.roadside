d35a4be40af8cbf212ed93a16650f8f4b9c5868b
Poloz <underpoloz@gmail.com>
2014-02-24 15:57:59 +0400

    Fuck it, i begin again.)

---
+++ src/org/roadside/actions/Shoot.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.roadside.actions;
+
+import org.roadside.items.Item;
+
+/**
+ *
+ * @author poloz@home
+ */
+public class Shoot {
+    public org.roadside.items.Item what, target; 
+    public org.roadside.beings.Being shooter;
+    
+    public void shoot (){
+        
+    }
+    
+}
